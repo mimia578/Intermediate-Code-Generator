2eae21e2677495f7e919b8c6ecb0cbaba50b0b93
mimia578 <mimia.mozammel@gmail.com>
2025-12-27 19:48:57 +0600

    bloop
    bloop

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# First pass: Generate AST and symbol table
-yacc -d -y --debug --verbose syntax_analyzer.y
+# Compiler build script for Lab 4
+yacc -d -y --debug --verbose 22101088_22101357.y
 echo 'Generated the parser C file and header file'
 g++ -w -c -o y.o y.tab.c
 echo 'Generated the parser object file'
-flex lex_analyzer.l
+flex 22101088_22101357.l
 echo 'Generated the scanner C file'
 g++ -fpermissive -w -c -o l.o lex.yy.c
 echo 'Generated the scanner object file'
-g++ y.o l.o -o two_pass_compiler
-echo 'All ready, running the two-pass compiler...'
+g++ y.o l.o -o compiler
+echo 'All ready, running the compiler...'
 
 # Run the compiler on the input file
-./two_pass_compiler input3.c
+./compiler input.c
 echo 'Compilation completed.'
 
 # Display output files
